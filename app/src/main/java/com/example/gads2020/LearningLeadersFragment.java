6c931c60280bbd84819a6ac1493fdd37e71f48af
Olamide Adeleye <adeleyeolamide56@gmail.com>
2020-09-15 07:24:04 +0100

    Init viewpager

---
+++ app/src/main/java/com/example/gads2020/LearningLeadersFragment.java
@@ -0,0 +1,6 @@
+package com.example.gads2020;
+
+import androidx.fragment.app.Fragment;
+
+public class LearningLeadersFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/gads2020/ViewPagerHomeAdapter.java
@@ -0,0 +1,24 @@
+package com.example.gads2020;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerHomeAdapter extends FragmentStateAdapter {
+
+    public ViewPagerHomeAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
