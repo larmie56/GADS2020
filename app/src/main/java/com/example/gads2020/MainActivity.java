bb35ee38311b12a898c6f1ea20c5dcf89b9bf2cd
Olamide Adeleye <adeleyeolamide56@gmail.com>
2020-09-15 07:13:18 +0100

    Implement splash screen

--- app/src/main/java/com/example/gads2020/MainActivity.java
+++ app/src/main/java/com/example/gads2020/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.gads2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager2 mViewPagerHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPagerHome = findViewById(R.id.viewpager_home);
     }
 }
